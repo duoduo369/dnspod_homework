e36b35e0338039db8771e3f02ff1f89085c539d4
duoduo369 <duoduo3369@gmail.com>
2014-06-03 19:18:00 +0800

    添加登录装饰器

--- app/dnspod_homework/auth/views.py
+++ app/dnspod_homework/auth/views.py
@@ -1,18 +1,58 @@
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from rest_framework.views import APIView
 from dnspod.apicn import UserDetail
 from rest_framework.response import Response
 from rest_framework.status import HTTP_401_UNAUTHORIZED
+from functools import wraps
+
+def login_required(func):
+    '''
+        need登录装饰器
+
+        如果之前没有登录，则返回401
+    '''
+    @wraps(func)
+    def __wrapper(*args, **kwargs):
+        request = args[1]
+        try:
+            request.session['email']
+            request.session['password']
+        except:
+            return Response('need login', HTTP_401_UNAUTHORIZED)
+        return func(*args, **kwargs)
+    return __wrapper
+
+def get_login_pair(request):
+    '''
+        返回登录属性组, 需要之前已经登录
+
+        {
+            email: 'XXX',
+            password: 'XXX',
+        }
+    '''
+    return dict(email=request.session['email'], password=request.session['password'])
+
 
 class AuthenticateView(APIView):
 
     def post(self, request):
+        '''
+            验证登录
+
+            require
+                email -- 邮箱
+                password -- 密码
+        '''
         params = request.DATA
         email = params.get('email', '')
         password = params.get('password', '')
         api = UserDetail(email=email, password=password)
         try:
             rep = api()
+            request.session['email'] = email
+            request.session['password'] = password
         except Exception as e:
             rep = e.message
             return Response(e.message, HTTP_401_UNAUTHORIZED)
