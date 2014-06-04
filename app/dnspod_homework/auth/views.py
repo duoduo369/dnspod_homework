185e91009c373f5063daa3b94365844d13d87b63
duoduo369 <duoduo3369@gmail.com>
2014-06-04 17:38:13 +0800

    后端验证改为base64方式，因为session状态存不住

--- app/dnspod_homework/auth/views.py
+++ app/dnspod_homework/auth/views.py
@@ -5,20 +5,22 @@ from dnspod.apicn import UserDetail
 from rest_framework.response import Response
 from rest_framework.status import HTTP_401_UNAUTHORIZED
 from functools import wraps
+import base64
 
 def login_required(func):
     '''
         need登录装饰器
 
         如果之前没有登录，则返回401
+
+        TODO:
+            use db, use django auth
     '''
     @wraps(func)
     def __wrapper(*args, **kwargs):
         request = args[1]
-        try:
-            request.session['email']
-            request.session['password']
-        except:
+        if not request.DATA.get('access_token', None) and \
+            not request.GET.get('access_token', None):
             return Response('need login', HTTP_401_UNAUTHORIZED)
         return func(*args, **kwargs)
     return __wrapper
@@ -26,13 +28,17 @@ def login_required(func):
 def get_login_pair(request):
     '''
         返回登录属性组, 需要之前已经登录
+        不使用数据库，因此用base64加密了密码
 
         {
             email: 'XXX',
             password: 'XXX',
         }
     '''
-    return dict(email=request.session['email'], password=request.session['password'])
+    access_token = request.DATA.get('access_token', None) or \
+             request.GET.get('access_token', None)
+    email, password = base64.decodestring(access_token).split('&&')
+    return dict(email=email, password=password)
 
 class LogoutView(APIView):
 
