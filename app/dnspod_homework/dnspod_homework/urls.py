2c33e58a439f2ad40ee701b6b67ad9fb0426cfb1
duoduo369 <duoduo3369@gmail.com>
2014-06-04 11:43:03 +0800

    关闭django默认的admin和auth

--- app/dnspod_homework/dnspod_homework/urls.py
+++ app/dnspod_homework/dnspod_homework/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dnspod_homework.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('auth.urls')),
     url(r'', include('domain.urls')),
     url(r'', include('record.urls')),
