78b0f4e80c62124a034b2a54122153db92495f35
duoduo369 <duoduo3369@gmail.com>
2014-06-05 15:49:02 +0800

    add gunicorn

---
+++ deploy/gunicorn/gunicorn_config.py
@@ -0,0 +1,18 @@
+import multiprocessing
+BASE_PATH = '/opt/dnspod_homework'
+bind = "0:8001"
+# workers = multiprocessing.cpu_count()
+# demo项目 1个worker
+workers = 1
+
+max_requests = 5000
+
+user = 'deployer'
+group = 'sudo'
+
+accesslog = BASE_PATH + '/deploy/gunicorn/log/gunicorn_access.log'
+errorlog = BASE_PATH + '/deploy/gunicorn/log/gunicorn_error.log'
+chdir = BASE_PATH + '/app/dnspod_homework'
+print 'accesslog:', accesslog
+print 'errorlog:', errorlog
+print 'chdir:', chdir
